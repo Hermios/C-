16dbc3af6f47063cc63bda3ff379b48d84531ab8
Hermios <nicoblex@yahoo.fr>
2016-11-11 11:57:45 +0000

    start service locator

---
+++ ServiceLocator/ServiceLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandardTools.ServiceLocator
+{
+    public class ServiceLocator : IServiceLocator
+    {
+        private static IServiceLocator _serviceLocator;
+
+        private ServiceLocator()
+        {
+
+        }
+        public static IServiceLocator getServiceLocator()
+        {
+            if (_serviceLocator == null)
+                _serviceLocator = new ServiceLocator();
+            return _serviceLocator;
+        }
+    }
+}
