ca07de5cf2dc12852cdb5404aec8910c53c055fd
Hermios <nicoblex@yahoo.fr>
2016-11-15 18:30:30 +0000

    Add Windowhandler and viewmodelbase+dll

--- ViewHandler/ViewModelBase.cs
+++ ViewHandler/ViewModelBase.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace StandardTools.ViewModel
+namespace StandardTools.ViewHandler
 {
     public abstract class ViewModelBase : INotifyPropertyChanged
     {
---
+++ ViewHandler/WindowHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace StandardTools.ViewHandler
+{
+    public class WindowHandler
+    {
+        public static void OpenWindow<T>(ViewModelBase viewModelBase,bool asyncShow) where T : Window
+        {
+            T view = (T)Activator.CreateInstance(typeof(T));
+            view.DataContext = viewModelBase;
+            if (asyncShow) view.Show();
+            else view.ShowDialog();
+        }
+    }
+}
